52b170a3032cda86f546f2fd7ba3a1e45c69dcf8
felix-b <felixberman76@gmail.com>
2015-01-31 08:32:53 +0200

    Quartz-based jobs WIP

---
+++ Source/NWheels.Puzzle.QuartzNet/QuartzJobContextAdapter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NWheels.Processing;
+using System.Threading;
+using Quartz;
+
+namespace NWheels.Puzzle.QuartzNet
+{
+    internal class QuartzJobContextAdapter : IApplicationJobContext
+    {
+        private readonly IJobExecutionContext _quartzContext;
+        private readonly CancellationTokenSource _cancellationSource;
+        private readonly CancellationToken _cancellationToken;
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public QuartzJobContextAdapter(IJobExecutionContext quartzContext)
+        {
+            _quartzContext = quartzContext;
+            _cancellationSource = new CancellationTokenSource();
+            _cancellationToken = _cancellationSource.Token;
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public void Report(string statusText, decimal percentCompleted)
+        {
+            //
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public bool IsDryRun
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public CancellationToken Cancellation
+        {
+            get
+            {
+                return _cancellationToken;
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public void Interrupt()
+        {
+            _cancellationSource.Cancel(throwOnFirstException: false);
+        }
+    }
+}
