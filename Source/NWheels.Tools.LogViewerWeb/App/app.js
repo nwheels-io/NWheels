e3f6674f7fb942703a339b389d71e5f417bff565
felix-b <felixberman76@gmail.com>
2015-03-03 07:36:43 +0200

    Enhanced log viewer page design - ready to add capture function

--- Source/NWheels.Tools.LogViewerWeb/App/app.js
+++ Source/NWheels.Tools.LogViewerWeb/App/app.js
@@ -1,15 +1,12 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('logViewerApp', ['ui.tree'])
-    .controller('threadLogController', ['$http', '$location', '$document', '$scope', function ($http, $location, $document, $scope) {
+    var app = angular.module('logViewerApp', ['ui.tree', 'isteven-multi-select', 'toggle-switch']);
 
-        $http.get($location.absUrl() + '/json').then(function(result) {
-			$document[0].title = 'Thread ' + result.data.logId;
-            $scope.threadLog = result.data;
-            $scope.rootNodes = [result.data.rootActivity];
-        });
-        
+	//-----------------------------------------------------------------------------------------------------------------
+    
+	app.controller('threadLogController', ['$http', '$location', '$document', '$scope', function ($http, $location, $document, $scope) {
+
         $scope.treeOptions = {
 			dataDragEnabled: false
         };
@@ -17,6 +14,72 @@
         $scope.toggle = function (scope) {
             scope.toggle();
         };
-    }]);
 
+        $scope.toggleDetails = function (scope) {
+            if(scope.$parent.expandedFullDetailsIndex===scope.$parent.$id) {
+				scope.$parent.expandedFullDetailsIndex = -1;
+			}
+			else {
+				scope.$parent.expandedFullDetailsIndex = scope.$parent.$id;
+			}
+        };
+
+        $scope.fullDetailsVisible = function (scope) {
+            return(scope.$parent.expandedFullDetailsIndex===scope.$parent.$id);
+        };
+
+        $scope.availableEnvironments = [
+			{name:'QA2', type:'QA', selected: true},
+			{name:'QA1', type:'QA', selected: true},
+			{name:'PROD', type:'PROD', selected: true}
+		];
+        $scope.availableThreads = [
+			{name:'StartUp', value:1},
+			{name:'ShutDown', value:2},
+			{name:'IncomingRequest', value:3, selected:true},
+			{name:'QueuedWorkItem', value:4, selected:true},
+			{name:'ScheduledJob', value:5, selected:true},
+			{name:'LogProcessing', value:6},
+			{name:'Unspecified', value:0}
+		];
+        $scope.availableNodes = [
+			{name:'Backend', selected: true},
+			{name:'AdvertiserApp'}
+		];
+        $scope.availableLogLevels = [
+			{name:'Debug'},
+			{name:'Verbose'},
+			{name:'Info', selected: true},
+			{name:'Warning', selected: true},
+			{name:'Error', selected: true},
+			{name:'Critical', selected: true}
+		];
+        $scope.selectedEnvironments = [];
+        $scope.selectedThreads = [];
+        $scope.selectedNodes = [];
+        $scope.selectedLogLevels = [];
+
+		$scope.logLevelSelectorLabels = {
+			selectAll       : "All",
+			selectNone      : "None",
+			reset           : "Undo",
+			search          : "Type here to search...",
+			nothingSelected : "All Threads"
+		};
+		
+		$scope.rootNodes = [];
+		
+        $http.get($location.absUrl() + '/json').then(function(result) {
+			$document[0].title = 'Thread ' + result.data.logId;
+            $scope.threadLog = result.data;
+            $scope.rootNodes.push(result.data.rootActivity);
+        });
+        
+        $http.get($location.absUrl() + '/json').then(function(result) {
+			$document[0].title = 'Thread ' + result.data.logId;
+            $scope.threadLog = result.data;
+            $scope.rootNodes.push(result.data.rootActivity);
+        });
+    }]);
+	
 })();
