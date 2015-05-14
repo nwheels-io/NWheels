a65fce6a92db077a524bcd7c3a33122ef09e9ceb
felixb <felixb@gmail.com>
2015-05-14 05:40:02 +0300

    Starting implementation of MongoDB data repository

---
+++ Source/NWheels.Puzzle.MongoDb/Impl/MongoDataRepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MongoDB.Driver;
+using NWheels.Entities.Core;
+
+namespace NWheels.Puzzle.MongoDb.Impl
+{
+    public abstract class MongoDataRepositoryBase : DataRepositoryBase
+    {
+        private readonly MongoDatabase _database;
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        protected MongoDataRepositoryBase(bool autoCommit, MongoDatabase database)
+            : base(autoCommit)
+        {
+            _database = database;
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        protected override void OnCommitChanges()
+        {
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        protected override void OnRollbackChanges()
+        {
+            throw new NotSupportedException();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        internal MongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        internal MongoDatabase Database
+        {
+            get { return _database; }
+        }
+    }
+}
---
+++ Source/NWheels.Puzzle.MongoDb/Impl/MongoQueryExpressionSpecializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace NWheels.Puzzle.MongoDb.Impl
+{
+    public static class MongoQueryExpressionSpecializer
+    {
+        public static Expression Specialize(Expression general)
+        {
+            return general;
+        }
+    }
+}
