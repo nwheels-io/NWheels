5054c7342d49b89e946e0962c940307cf8f2d2a7
felix-b <felixberman76@gmail.com>
2015-05-24 14:58:23 +0300

    Adding method IRuleEngine.DescribeRuleSystem

--- Source/NWheels/Processing/Rules/IRuleEngine.cs
+++ Source/NWheels/Processing/Rules/IRuleEngine.cs
@@ -4,6 +4,11 @@ namespace NWheels.Processing.Rules
 {
     public interface IRuleEngine
     {
+        RuleSystemDescription DescribeRuleSystem<TCodeBehind, TDataContext>()
+             where TCodeBehind : IRuleSystemCodeBehind<TDataContext>;
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+        
         CompiledRuleSystem<TDataContext> CompileRuleSystem<TCodeBehind, TDataContext>(RuleSystemData rules)
             where TCodeBehind : IRuleSystemCodeBehind<TDataContext>;
     }
