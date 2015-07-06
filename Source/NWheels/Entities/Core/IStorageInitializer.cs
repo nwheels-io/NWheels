c4555fb6bcf9cf890f4c2815fee10640d44ca679
felix-b <felixberman76@gmail.com>
2015-07-06 09:47:22 +0300

    Upgrading to Hapil 0.9.9 (for 0.9.8 was a broken package)

---
+++ Source/NWheels/Entities/Core/IStorageInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWheels.Entities.Core
+{
+    public interface IStorageInitializer
+    {
+        void MigrateStorageSchema(string connectionString);
+        void CreateStorageSchema(string connectionString);
+        void DropStorageSchema(string connectionString);
+    }
+}
