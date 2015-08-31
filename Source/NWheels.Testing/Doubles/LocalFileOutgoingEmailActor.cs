d3c6e49784538a597004171962aedebc56c7a318
felix-b <felixberman76@gmail.com>
2015-08-31 09:40:50 +0300

    Email sender test double - saving outgoing emails to files

---
+++ Source/NWheels.Testing/Doubles/LocalFileOutgoingEmailActor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NWheels.Processing.Messages;
+using NWheels.Utilities;
+
+namespace NWheels.Testing.Doubles
+{
+    public class LocalFileOutgoingEmailActor : IMessageHandler<OutgoingEmailMessage>
+    {
+        private readonly string _outputFolderPath;
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public LocalFileOutgoingEmailActor()
+        {
+            _outputFolderPath = PathUtility.HostBinPath("Email.Out");
+
+            if ( !Directory.Exists(_outputFolderPath) )
+            {
+                Directory.CreateDirectory(_outputFolderPath);
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        #region Implementation of IMessageHandler<OutgoingEmailMessage>
+
+        public void HandleMessage(OutgoingEmailMessage message)
+        {
+            string subject;
+            string body;
+            message.FormatTemplates(out subject, out body);
+
+            var output = new StringBuilder();
+
+            output.AppendFormat("-- UTC DATE/TIME\r\n{0:yyyy-MM-dd HH:mm:ss}\r\n", DateTime.UtcNow);
+            output.AppendFormat("-- TO\r\n{0}", string.Join("", message.To.Select(r => r.PersonName + " <" + r.EmailAddress + ">\r\n")));
+            output.AppendFormat("-- CC\r\n{0}", string.Join("", message.Cc.Select(r => r.PersonName + " <" + r.EmailAddress + ">\r\n")));
+            output.AppendFormat("-- BCC\r\n{0}", string.Join("", message.Bcc.Select(r => r.PersonName + " <" + r.EmailAddress + ">\r\n")));
+            output.AppendFormat("-- SUBJECT\r\n{0}\r\n", subject);
+            output.AppendFormat("-- BODY\r\n{0}\r\n", body);
+
+            Thread.Sleep(10);
+
+            var fileName = string.Format("{0:yyyy-MM-dd-HHmm-ssfff}.txt", DateTime.UtcNow);
+            File.WriteAllText(Path.Combine(_outputFolderPath, fileName), output.ToString());
+        }
+
+        #endregion
+    }
+}
---
+++ Source/NWheels/Processing/Messages/OutgoingEmailMessage.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartFormat;
+
+namespace NWheels.Processing.Messages
+{
+    public class OutgoingEmailMessage : MessageObjectBase
+    {
+        public OutgoingEmailMessage()
+        {
+            this.To = new List<Recipient>();
+            this.Cc = new List<Recipient>();
+            this.Bcc = new List<Recipient>();
+            this.Attachments = new List<Attachment>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public void FormatTemplates(out string formattedSubject, out string formattedHtmlBody)
+        {
+            string subjectTemplate;
+            string bodyTemplate;
+
+            if ( SubjectAtBodyTemplateFirstLine )
+            {
+                using ( var reader = new StringReader(BodyHtmlTemplate) )
+                {
+                    subjectTemplate = reader.ReadLine();
+                    bodyTemplate = reader.ReadToEnd();
+                }
+            }
+            else
+            {
+                subjectTemplate = Subject;
+                bodyTemplate = BodyHtmlTemplate;
+            }
+
+            formattedSubject = Smart.Format(subjectTemplate, this.TemplateData);
+            formattedHtmlBody = Smart.Format(bodyTemplate, this.TemplateData);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public List<Recipient> To { get; private set; }
+        public List<Recipient> Cc { get; private set; }
+        public List<Recipient> Bcc { get; private set; }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Specifies the subject line of the email. Can contain value placeholders with expressions evaluated on the TemplateData property.
+        /// Note: this property is ignored if SubjectAtBodyTemplateFirstLine is true.
+        /// </summary>
+        public string Subject { get; set; }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Specifies that first line of the body template should be treated as subject line. If true, Subject property is ignored, and the body starts 
+        /// on the second line of the template.
+        /// </summary>
+        public bool SubjectAtBodyTemplateFirstLine { get; set; }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// The HTML contents of the email. Can contain value placeholders with expressions evaluated on the TemplateData property.
+        /// </summary>
+        public string BodyHtmlTemplate { get; set; }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// An object that contains values for placeholders in the template.
+        /// </summary>
+        public object TemplateData { get; set; }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Attachments to the email
+        /// </summary>
+        public List<Attachment> Attachments { get; private set; }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class Recipient
+        {
+            public Recipient(string personName, string emailAddress)
+            {
+                PersonName = personName;
+                EmailAddress = emailAddress;
+            }
+
+            //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+            public string PersonName { get; private set; }
+            public string EmailAddress { get; private set; }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class Attachment
+        {
+            public Attachment(string fileName, byte[] fileContents)
+            {
+                this.FileName = fileName;
+                this.FileExtension = Path.GetExtension(fileName);
+                this.FileContents = fileContents;
+            }
+
+            //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+            public Attachment(string fileName, Stream fileContents)
+            {
+                this.FileName = fileName;
+                this.FileExtension = Path.GetExtension(fileName);
+                
+                using ( var buffer = new MemoryStream() )
+                {
+                    fileContents.CopyTo(buffer);
+                    this.FileContents = buffer.ToArray();
+                }
+            }
+
+            //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+            public string FileName { get; private set; }
+            public string FileExtension { get; private set; }
+            public byte[] FileContents { get; private set; }
+        }
+    }
+}
