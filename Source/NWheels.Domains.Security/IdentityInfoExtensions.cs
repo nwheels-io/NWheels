9f3e769d3bdca683252c4fe169b1a94de669ab82
felix-b <felixberman76@gmail.com>
2015-09-11 18:02:43 +0300

    Adding ability to get user account from session

---
+++ Source/NWheels.Domains.Security/IdentityInfoExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NWheels.Authorization;
+using NWheels.Domains.Security.Core;
+
+namespace NWheels.Domains.Security
+{
+    public static class IdentityInfoExtensions
+    {
+        public static IUserAccountEntity GetUserAccount(this IIdentityInfo identityInfo)
+        {
+            return ((UserAccountIdentity)identityInfo).GetUserAccount();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public static TUserAccount GetUserAccount<TUserAccount>(this IIdentityInfo identityInfo) where TUserAccount : IUserAccountEntity
+        {
+            return (TUserAccount)((UserAccountIdentity)identityInfo).GetUserAccount();
+        }
+    }
+}
