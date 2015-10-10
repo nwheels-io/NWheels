4ba3e33bb16483fe44968224f563271932fb9a1d
felix-b <felixberman76@gmail.com>
2015-10-10 08:58:20 +0300

    Adding logger to Breeze REST endpoint

---
+++ Source/NWheels.Stacks.ODataBreeze/IBreezeEndpointLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NWheels.Logging;
+
+namespace NWheels.Stacks.ODataBreeze
+{
+    public interface IBreezeEndpointLogger : IApplicationEventLogger
+    {
+        [LogActivity]
+        ILogActivity RestWriteInProgress();
+
+        [LogInfo]
+        void RestWriteCompleted();
+
+        [LogError]
+        void RestWriteFailed(Exception e);
+    }
+}
