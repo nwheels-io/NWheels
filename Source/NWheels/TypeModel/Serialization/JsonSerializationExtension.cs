335e56f9b129db03596d6f12c27ab9133feefe2d
felix-b <felixberman76@gmail.com>
2016-02-14 19:51:30 +0200

    Json serialization: adding extension to plug in handling of base types (e.g. Money, Interval) - WIP

---
+++ Source/NWheels/TypeModel/Serialization/JsonSerializationExtension.cs
@@ -0,0 +1,48 @@
+﻿#if false
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using NWheels.UI.Core;
+
+namespace NWheels.TypeModel.Serialization
+{
+    public class JsonSerializationExtension : IJsonSerializationExtension
+    {
+        #region Implementation of IJsonSerializationExtension
+
+        public void ApplyTo(JsonSerializerSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class IntervalContractResolver : DefaultContractResolver
+        {
+            #region Overrides of DefaultContractResolver
+
+            protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+            {
+                return new[] {
+                    new JsonProperty() {
+                        PropertyName = 
+                    },
+                    new JsonProperty() {
+                        
+                    } 
+                };
+            }
+
+            #endregion
+        }
+    }
+}
+
+#endif
