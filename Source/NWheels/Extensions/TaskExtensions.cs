732775333985e6648c3e480bc313f16c34ecd814
felix-b <felixberman76@gmail.com>
2016-06-13 16:25:23 +0300

    Core/Extensions: adding extension method Task.WaitOrThrow.

---
+++ Source/NWheels/Extensions/TaskExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NWheels.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static void WaitOrThrow(this Task task, TimeSpan timeout, CancellationToken? cancellation = null)
+        {
+            WaitOrThrow(task, (int)timeout.TotalMilliseconds, cancellation);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public static void WaitOrThrow(this Task task, int millisecondsTimeout, CancellationToken? cancellation = null)
+        {
+            if (!task.Wait(millisecondsTimeout, cancellation.GetValueOrDefault(CancellationToken.None)))
+            {
+                throw new TimeoutException("Timed out waiting for a Task to complete.");
+            }
+        }
+    }
+}
