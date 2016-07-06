716ff4d3944f3e87bd7f96757d16845226f5bc7d
felix-b <felixberman76@gmail.com>
2016-07-06 17:24:22 +0300

    Stacks/Apis/MicrosoftTranslator: adding integration with Microsoft Translator service and Bulk Translate operation.

---
+++ Source/NWheels.Stacks.Apis.MicrosoftTranslator/ModuleLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using NWheels.Extensions;
+using NWheels.Globalization.Core;
+using NWheels.Stacks.Apis.MicrosoftTranslator.Domain;
+using NWheels.Stacks.Apis.MicrosoftTranslator.UI;
+
+namespace NWheels.Stacks.Apis.MicrosoftTranslator
+{
+    public class ModuleLoader : Autofac.Module
+    {
+        #region Overrides of Module
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.NWheelsFeatures()
+                .UI()
+                .RegisterUidlExtension<LocaleCrudScreenPart, LocaleCrudScreenPartExtension>();
+            
+            builder.NWheelsFeatures()
+                .Processing()
+                .RegisterTransactionScript<LocaleBulkTranslateTx>();
+        }
+
+        #endregion
+    }
+}
---
+++ Source/NWheels.Stacks.Apis.MicrosoftTranslator/UI/LocaleCrudScreenPartExtension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NWheels.Globalization.Core;
+using NWheels.Stacks.Apis.MicrosoftTranslator.Domain;
+using NWheels.UI;
+using NWheels.UI.Toolbox;
+using NWheels.UI.Uidl;
+
+namespace NWheels.Stacks.Apis.MicrosoftTranslator.UI
+{
+    public class LocaleCrudScreenPartExtension : 
+        UidlExtension<LocaleCrudScreenPart, Empty.Data, LocaleCrudScreenPart.IState>
+    {
+        #region Overrides of UidlScreenPartExtension<LocaleCrudScreenPart,Data,CrudScreenPart<IApplicationLocaleEntity>.IState>
+
+        protected override void ExtendPresenter(
+            LocaleCrudScreenPart screenPart, 
+            PresenterBuilder<LocaleCrudScreenPart, Empty.Data, CrudScreenPart<IApplicationLocaleEntity>.IState> presenter)
+        {
+            screenPart.Crud.StaticCommands.Add(BulkTranslate);
+            screenPart.PopupContents.Add(BulkTranslateTx);
+
+            BulkTranslateTx.AttachAsPopupTo(presenter, BulkTranslate);
+            BulkTranslateTx.UseOutputForm();
+            BulkTranslateTx.UseOKCancelNotation(withIcons: true);
+
+            BulkTranslate.Severity = CommandSeverity.Change;
+            BulkTranslate.Icon = "gear";
+
+            BulkTranslateTx.InputForm.UseSectionsInsteadOfTabs();
+            BulkTranslateTx.InputForm.Field(x => x.TranslateFrom, 
+                type: FormFieldType.Lookup, 
+                modifiers: FormFieldModifiers.DropDown | FormFieldModifiers.LookupShowSelectNone);
+            BulkTranslateTx.InputForm.Field(x => x.TranslateTo, 
+                type: FormFieldType.Lookup,
+                modifiers: FormFieldModifiers.DropDown | FormFieldModifiers.LookupShowSelectNone);
+
+            BulkTranslateTx.InputForm.Field(x => x.MicrosoftTranslatorServiceAuthentication, setup: f => {
+                var authForm = (Form<LocaleBulkTranslateTx.IMicrosoftTranslatorAuthenticationEntityPart>)f.NestedWidget;
+                authForm.IsModalPopup = true;
+            });
+
+            BulkTranslateTx.OutputForm.IsModalPopup = true;
+            BulkTranslateTx.OutputForm.Field(x => x.Details,
+                type: FormFieldType.Label,
+                modifiers: FormFieldModifiers.Memo | FormFieldModifiers.FlatStyle);
+        }
+
+        #endregion
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public UidlCommand BulkTranslate { get; set; }
+        
+        public TransactionForm<
+            LocaleBulkTranslateTx.IInput, 
+            LocaleBulkTranslateTx, 
+            LocaleBulkTranslateTx.IOutput> BulkTranslateTx { get; set; }
+    }
+}
