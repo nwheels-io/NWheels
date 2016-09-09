7ca0aa7b8f7f83bb2ebd2e7a78f38781f1787e43
felix-b <felixberman76@gmail.com>
2016-09-09 08:28:26 +0300

    Samples: made HR example app successfully load

--- Source/NWheels.Samples.MyHRApp/ModuleLoader.cs
+++ Source/NWheels.Samples.MyHRApp/ModuleLoader.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Autofac;
+using NWheels.Domains.Security;
+using NWheels.Extensions;
+using NWheels.Samples.MyHRApp.Deployment;
+using NWheels.Samples.MyHRApp.Domain;
+using NWheels.Samples.MyHRApp.UI;
+using NWheels.Stacks.MongoDb;
 
 namespace NWheels.Samples.MyHRApp
 {
@@ -12,7 +18,14 @@ namespace NWheels.Samples.MyHRApp
 
         protected override void Load(ContainerBuilder builder)
         {
-            base.Load(builder);
+            builder.NWheelsFeatures().Entities().RegisterDataPopulator<HRContextPopulator>().FirstInPipeline();
+            builder.NWheelsFeatures().Entities().RegisterDataRepository<IHRContext>().WithInitializeStorageOnStartup();
+
+            builder.NWheelsFeatures().ObjectContracts().Concretize<IUserAccountDataRepository>().With<IHRContext>();
+            builder.NWheelsFeatures().ObjectContracts().Concretize<IAutoIncrementIdDataRepository>().With<IHRContext>();
+
+            builder.NWheelsFeatures().Processing().RegisterTransactionScript<HRLoginTx>();
+            builder.NWheelsFeatures().UI().RegisterApplication<HRApp>().WithWebEndpoint();
         }
 
         #endregion
