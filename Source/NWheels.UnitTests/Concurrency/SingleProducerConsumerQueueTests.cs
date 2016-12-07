848f2b2e4b65709d9098ca9e09b18429828a5394
felix-b <felixberman76@gmail.com>
2016-12-07 23:13:51 +0200

    Core/Concurrency: adding high-throughput low-latency single-producer-single-consumer queue 'SingleProducerConsumerQueue'  (more tests are to be added)

---
+++ Source/NWheels.UnitTests/Concurrency/SingleProducerConsumerQueueTests.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NWheels.Concurrency;
+using Shouldly;
+
+namespace NWheels.UnitTests.Concurrency
+{
+    [TestFixture]
+    public class SingleProducerConsumerQueueTests
+    {
+        [Test]
+        public void CanEnqueueAndDequeue()
+        {
+            //- arrane
+
+            var queue = new SingleProducerConsumerQueue<int>(capacity: 100);
+
+            queue.TryEnqueue(111, TimeSpan.MaxValue, CancellationToken.None).ShouldBe(true);
+            queue.TryEnqueue(222, TimeSpan.MaxValue, CancellationToken.None).ShouldBe(true);
+            queue.TryEnqueue(333, TimeSpan.MaxValue, CancellationToken.None).ShouldBe(true);
+
+            //- act & assert
+
+            int dequeued1, dequeued2, dequeued3;
+            
+            queue.TryDequeue(out dequeued1, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryDequeue(out dequeued2, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryDequeue(out dequeued3, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+
+            dequeued1.ShouldBe(111);
+            dequeued2.ShouldBe(222);
+            dequeued3.ShouldBe(333);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+        
+        [Test]
+        public void CannotDequeueFromEmptyQueue()
+        {
+            //- arrange
+
+            var queue = new SingleProducerConsumerQueue<int>(capacity: 100);
+
+            //- act
+
+            int value;
+            var wasDequeued = queue.TryDequeue(out value, TimeSpan.FromMilliseconds(50), CancellationToken.None);
+
+            //- assert
+
+            wasDequeued.ShouldBe(false);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Test]
+        public void CannotDequeueFromEmptiedQueue()
+        {
+            //- arrange
+
+            var queue = new SingleProducerConsumerQueue<int>(capacity: 100);
+            int dequeuedValue;
+
+            queue.TryEnqueue(111, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryEnqueue(222, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+
+            queue.TryDequeue(out dequeuedValue, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryDequeue(out dequeuedValue, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+
+            //- act
+
+            var wasDequeued = queue.TryDequeue(out dequeuedValue, TimeSpan.FromMilliseconds(50), CancellationToken.None);
+
+            //- assert
+
+            wasDequeued.ShouldBe(false);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Test]
+        public void CannotEnqueueMoreThanCapacity()
+        {
+            //- arrange
+
+            var queue = new SingleProducerConsumerQueue<int>(capacity: 3);
+
+            queue.TryEnqueue(111, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryEnqueue(222, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryEnqueue(333, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+
+            //- act
+
+            var wasEnqueued = queue.TryEnqueue(444, TimeSpan.FromMilliseconds(50), CancellationToken.None);
+
+
+            //- assert
+
+            wasEnqueued.ShouldBe(false);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Test]
+        public void EnqueueToFullBufferBlocksUntilNextDequeue()
+        {
+            //- arrange
+
+            var queue = new SingleProducerConsumerQueue<int>(capacity: 3);
+
+            queue.TryEnqueue(111, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryEnqueue(222, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryEnqueue(333, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+
+            int dequeuedValue = 0;
+            var clock = Stopwatch.StartNew();
+            var dequeueTask = Task.Factory.StartNew(() => {
+                Thread.Sleep(1000);
+                queue.TryDequeue(out dequeuedValue, TimeSpan.Zero, CancellationToken.None);
+            });
+
+            //- act
+
+            var wasEnqueued = queue.TryEnqueue(444, TimeSpan.FromSeconds(10), CancellationToken.None);
+
+            //- assert
+
+            wasEnqueued.ShouldBe(true);
+            clock.ElapsedMilliseconds.ShouldBeGreaterThanOrEqualTo(1000);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Test]
+        public void DequeueFromEmptyBufferBlocksUntilNextEnqueue()
+        {
+            //- arrange
+
+            var queue = new SingleProducerConsumerQueue<int>(capacity: 3);
+
+            var clock = Stopwatch.StartNew();
+            var enqueueTask = Task.Factory.StartNew(() => {
+                Thread.Sleep(1000);
+                queue.TryEnqueue(123, TimeSpan.Zero, CancellationToken.None);
+            });
+
+            //- act
+
+            int dequeuedValue;
+            var wasDequeued = queue.TryDequeue(out dequeuedValue, TimeSpan.FromSeconds(10), CancellationToken.None);
+
+            //- assert
+
+            wasDequeued.ShouldBe(true);
+            dequeuedValue.ShouldBe(123);
+            clock.ElapsedMilliseconds.ShouldBeGreaterThanOrEqualTo(1000);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Test]
+        public void DequeueFromEmptiedBufferBlocksUntilNextEnqueue()
+        {
+            //- arrange
+
+            var queue = new SingleProducerConsumerQueue<int>(capacity: 3);
+            int dequeuedValue;
+
+            queue.TryEnqueue(111, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryEnqueue(222, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+
+            queue.TryDequeue(out dequeuedValue, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+            queue.TryDequeue(out dequeuedValue, TimeSpan.Zero, CancellationToken.None).ShouldBe(true);
+
+            var clock = Stopwatch.StartNew();
+            var enqueueTask = Task.Factory.StartNew(() => {
+                Thread.Sleep(1000);
+                queue.TryEnqueue(333, TimeSpan.Zero, CancellationToken.None);
+            });
+
+            //- act
+
+            var wasDequeued = queue.TryDequeue(out dequeuedValue, TimeSpan.FromSeconds(10), CancellationToken.None);
+
+            //- assert
+
+            wasDequeued.ShouldBe(true);
+            dequeuedValue.ShouldBe(333);           
+            clock.ElapsedMilliseconds.ShouldBeGreaterThanOrEqualTo(1000);
+        }
+    }
+}
---
+++ Source/NWheels/Concurrency/SingleProducerConsumerQueue.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NWheels.Concurrency
+{
+    //
+    // Single producer/single consumer lock-free FIFO queue, based on ring buffer and spin-waits.
+    //
+    internal class SingleProducerConsumerQueue<T>
+    {
+        // lenth of the ring buffer
+        private readonly long _capacity;
+
+        // the ring buffer
+        private readonly T[] _buffer;
+
+        // tracks enqueue operation timeout; 
+        // one instance is reused by all Enqueue operations
+        private readonly Stopwatch _enqueueClock;
+
+        // tracks dequeue operation timeout; 
+        // one instance is reused by all Dequeue operations
+        private readonly Stopwatch _dequeueClock;
+
+        // position of last enqueued item, initially -1
+        // it is ever-increasing: modulus capacity to determine actual index in buffer; 
+        // this field is only mutated by a single producer thread
+        private long _head;
+
+        // position of last dequeued item, initially -1
+        // it is ever-increasing: modulus capacity to determine actual index in buffer;
+        // this field is only mutated by a single consumer thread
+        private long _tail;
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+        //
+        // Initializes new instance of the queue with specified capacity of the ring buiffer.
+        //
+        public SingleProducerConsumerQueue(long capacity)
+        {
+            _capacity = capacity;
+            _buffer = new T[capacity];
+            _enqueueClock = Stopwatch.StartNew();
+            _dequeueClock = Stopwatch.StartNew();
+            _head = -1; // first enqueued item will be at position 0
+            _tail = -1; // first dequeued item will be at position 0
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+        //
+        // Enqueues an item into the queue.
+        // This method blocks until a slot in buffer is available, timeout expires, or cancellation token is signaled.
+        // Returns true if an item was successfully dequeued; false if either timeout expired or cancellation signaled.
+        //
+        public bool TryEnqueue(T item, TimeSpan timeout, CancellationToken cancel)
+        {
+            // check if we have space in buffer for another item
+            if (_head - _tail >= _capacity)
+            {
+                // the buffer is full. we have to wait until the consumer dequeues at least one item
+                // use combination of spin-wait/yield (implemented by SpinWait struct) until at least one slot becomes available
+
+                var startTime = _enqueueClock.Elapsed;
+                var spin = new SpinWait();
+                var spinCount = 0;
+
+                while (_head - _tail >= _capacity)
+                {
+                    spinCount++;
+                    spin.SpinOnce(); // each time SpinOnce will decide to either spin or yield
+
+                    // timeout/cancellation check costs some CPU cycles
+                    // because of that, we check for timeout/cancellation once every 100 iterations
+                    if ((spinCount % 100) == 0 && (cancel.IsCancellationRequested || (_dequeueClock.Elapsed.Subtract(startTime) >= timeout)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // now we are sure we have space for the new item
+            _buffer[(_head + 1) % _capacity] = item;
+
+            // now it is safe to let Dequeue read the enqueued slot, so we increment the _head
+            // there is no race condition in incrementing the _head; we use Interlocked to prevent instruction reordering optimizations
+            Interlocked.Increment(ref _head);
+
+            return true;
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+        //
+        // Dequeues an item from the queue.
+        // This method blocks until an item is available, timeout expires, or cancellation is signaled.
+        // Returns true if an item was successfully enqueued; false if either timeout expired or cancellation signaled.
+        //
+        public bool TryDequeue(out T item, TimeSpan timeout, CancellationToken cancel)
+        {
+            // check if we have at least one item in the queue
+            if (_tail >= _head)
+            {
+                // the buffer is empty. we have to wait until the producer enqueues at least one item
+                // use combination of spin-wait/yield (implemented by SpinWait struct) until at least one item  becomes available
+
+                var startTime = _dequeueClock.Elapsed;
+                var spin = new SpinWait();
+                var spinCount = 0;
+
+                while (_tail >= _head)
+                {
+                    spinCount++;
+                    spin.SpinOnce(); // each time SpinOnce will decide to either spin or yield
+
+                    // timeout/cancellation check costs some CPU cycles
+                    // because of that, we check for timeout/cancellation once every 100 iterations
+                    if ((spinCount % 100) == 0 && (cancel.IsCancellationRequested || (_dequeueClock.Elapsed.Subtract(startTime) >= timeout)))
+                    {
+                        item = default(T);
+                        return false;
+                    }
+                }
+            }
+
+            // now we are sure we have at least one item in buffer, so we read it
+            item = _buffer[(_tail + 1) % _capacity];
+
+            // now it is safe to let Enqueue overwrite the dequeued slot, so we increment the _tail
+            // there is no race condition in incrementing the _tail; we use Interlocked to prevent instruction reordering optimizations
+            Interlocked.Increment(ref _tail);
+
+            return true;
+        }
+    }
+}
