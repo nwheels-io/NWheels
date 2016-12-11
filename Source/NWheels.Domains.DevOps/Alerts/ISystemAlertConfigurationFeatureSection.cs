67423a4b891be7c04314293b3f7ce21b5ba6e824
eliturteltau <eliturteltau@gmail.com>
2016-12-11 08:56:41 +0200

    DevOps: SystemAlerts: Defined ISystemAlertConfigurationFeatureSection

---
+++ Source/NWheels.Domains.DevOps/Alerts/ISystemAlertConfigurationFeatureSection.cs
@@ -0,0 +1,15 @@
+﻿using NWheels.Configuration;
+
+namespace NWheels.Domains.DevOps.Alerts
+{
+    [ConfigurationSection(XmlName = "SystemAlerts")]
+    public interface ISystemAlertConfigurationFeatureSection : IConfigurationSection
+    {
+        INamedObjectCollection<ISystemAlertConfigurationElement> SystemAlerts { get; }
+    }
+
+    [ConfigurationElement(XmlName = "SystemAlert")]
+    public interface ISystemAlertConfigurationElement : INamedConfigurationElement
+    {
+    }
+}
---
+++ Source/NWheels.Domains.DevOps/Alerts/SystemAlertConfigurationFeatureLoader.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using NWheels.Extensions;
+
+namespace NWheels.Domains.DevOps.Alerts
+{
+    class SystemAlertConfigurationFeatureLoader : Module
+    {
+        #region Overrides of Module
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.NWheelsFeatures().Configuration().RegisterSection<ISystemAlertConfigurationFeatureSection>();
+        }
+
+        #endregion
+    }
+}
