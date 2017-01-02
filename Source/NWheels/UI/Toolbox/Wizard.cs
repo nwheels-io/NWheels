d6df43d02530aafa2b3e6bcf336e98933219d475
felix-b <felixberman76@gmail.com>
2017-01-02 10:08:24 +0200

    Core/UI + Web + Inspinia: adding Wizard widget (example will follow in Samples/MyMusicDB app).

---
+++ Source/NWheels/UI/Toolbox/Wizard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using NWheels.DataObjects;
+using NWheels.Extensions;
+using NWheels.Globalization.Core;
+using NWheels.UI;
+using NWheels.UI.Core;
+using NWheels.UI.Toolbox;
+using NWheels.UI.Uidl;
+
+namespace NWheels.UI.Toolbox
+{
+    public class Wizard : WidgetBase<Wizard, Empty.Data, Empty.State>
+    {
+        public Wizard(string idName, ControlledUidlNode parent)
+            : base(idName, parent)
+        {
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [DataMember]
+        public List<WidgetUidlNode> Pages { get; set; }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        protected override void DescribePresenter(PresenterBuilder<Wizard, Empty.Data, Empty.State> presenter)
+        {
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        protected override void OnBuild(UidlBuilder builder)
+        {
+            base.OnBuild(builder);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public override IEnumerable<WidgetUidlNode> GetNestedWidgets()
+        {
+            return base.GetNestedWidgets().Concat(Pages);
+        }
+    }
+}
