ffb81947b6ee72d8adf922c3479799401837e10e
felix-b <felixberman76@gmail.com>
2017-01-18 14:23:16 +0200

    Tools/Mongo: added utility to run simple bulk updates on MongoDB collections

---
+++ Tools/Mongo/MongoBulkUpdate461/UpdateOperator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoBulkUpdate461
+{
+    public enum UpdateOperator
+    {
+        Set,
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+}
