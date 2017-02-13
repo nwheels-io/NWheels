c4473276ed7d564dc56ba762142a03d32e6acfe2
felix-b <felixberman76@gmail.com>
2017-02-13 23:42:20 +0200

    Core/Extensions: added TrimPrefix and TrimSuffix string extensions.

---
+++ Source/NWheels.Api/Extensions/StringExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NWheels.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string TrimPrefix(this string source, string prefix)
+        {
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(prefix))
+            {
+                return source;
+            }
+
+            if (source.Length <= prefix.Length || !source.StartsWith(prefix))
+            {
+                return source;
+            }
+
+            return source.Substring(startIndex: prefix.Length);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public static string TrimSuffix(this string source, string suffix)
+        {
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(suffix))
+            {
+                return source;
+            }
+
+            if (source.Length <= suffix.Length || !source.EndsWith(suffix))
+            {
+                return source;
+            }
+
+            return source.Substring(startIndex: 0, length: source.Length - suffix.Length);
+        }
+    }
+}
---
+++ Source/NWheels.Core.UnitTests/Extensions/StringExtensionTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using NWheels.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NWheels.Core.UnitTests.Extensions
+{
+    public class StringExtensionTests
+    {
+        [Theory]
+        [InlineData("PrefixEndOfString", "Prefix", "EndOfString")]
+        [InlineData("PrefixEndOfString", "pREFIX", "PrefixEndOfString")]
+        [InlineData("EndOfString", "Prefix", "EndOfString")]
+        [InlineData("Prefix", "Prefix", "Prefix")]
+        [InlineData("", "Prefix", "")]
+        [InlineData(null, "Prefix", null)]
+        [InlineData("PrefixEndOfString", "", "PrefixEndOfString")]
+        [InlineData("PrefixEndOfString", null, "PrefixEndOfString")]
+        [InlineData("", "", "")]
+        [InlineData("", null, "")]
+        [InlineData(null, "", null)]
+        [InlineData(null, null, null)]
+        public void TestTrimPrefix(string input, string prefix, string expectedOutput)
+        {
+            //-- act
+
+            var actualOutput = input.TrimPrefix(prefix);
+
+            //-- assert
+
+            actualOutput.Should().Be(expectedOutput);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Theory]
+        [InlineData("BeginOfStringSuffix", "Suffix", "BeginOfString")]
+        [InlineData("BeginOfStringSuffix", "sUFFIX", "BeginOfStringSuffix")]
+        [InlineData("BeginOfString", "Suffix", "BeginOfString")]
+        [InlineData("Suffix", "Suffix", "Suffix")]
+        [InlineData("", "Suffix", "")]
+        [InlineData(null, "Suffix", null)]
+        [InlineData("BeginOfStringSuffix", "", "BeginOfStringSuffix")]
+        [InlineData("BeginOfStringSuffix", null, "BeginOfStringSuffix")]
+        [InlineData("", "", "")]
+        [InlineData("", null, "")]
+        [InlineData(null, "", null)]
+        [InlineData(null, null, null)]
+        public void TestTrimSuffixx(string input, string suffix, string expectedOutput)
+        {
+            //-- act
+
+            var actualOutput = input.TrimSuffix(suffix);
+
+            //-- assert
+
+            actualOutput.Should().Be(expectedOutput);
+        }
+    }
+}
