c81517bc51077012e65e4d92aa5b3c3c948566e7
ILyaIL83 <ilya.il83@gmail.com>
2017-03-14 15:56:17 +0200

    Platform/Messaging: Unit test project fixed;

---
+++ Source/NWheels.Platform.Messaging.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace NWheels.Platform.Messaging.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
