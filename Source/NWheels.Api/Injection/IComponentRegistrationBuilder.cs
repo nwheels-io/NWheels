fa20468328e0a53ea15065a42a6317b41c0990db
felix-b <felixberman76@gmail.com>
2017-04-26 19:02:05 +0300

    Kernel/Injection: adding missing changes

--- Source/NWheels.Api/Injection/IComponentRegistrationBuilder.cs
+++ Source/NWheels.Api/Injection/IComponentRegistrationBuilder.cs
@@ -6,10 +6,10 @@ namespace NWheels.Injection
 {
     public interface IComponentRegistrationBuilder
     {
-        IComponentRegistrationBuilder ForService<TService>();
-        IComponentRegistrationBuilder ForServices<TService1, TService2>();
-        IComponentRegistrationBuilder ForServices<TService1, TService2, TService3>();
-        IComponentRegistrationBuilder ForServices(params Type[] serviceTypes);
+        IComponentConditionBuilder ForService<TService>();
+        IComponentConditionBuilder ForServices<TService1, TService2>();
+        IComponentConditionBuilder ForServices<TService1, TService2, TService3>();
+        IComponentConditionBuilder ForServices(params Type[] serviceTypes);
     }
 
     //---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -19,5 +19,12 @@ namespace NWheels.Injection
         IComponentRegistrationBuilder SingleInstance();
         IComponentRegistrationBuilder InstancePerDependency();
         IComponentInstantiationBuilder WithParameter<T>(T value);
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public interface IComponentConditionBuilder : IComponentRegistrationBuilder
+    {
+        void AsFallback();
     }
 }
