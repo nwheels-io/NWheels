9604f0eeda1122953f269984138fa5cfce6e90e0
felix-b <felixberman76@gmail.com>
2017-04-27 23:37:24 +0300

    Kernel/Configuration: added configuration attributes

---
+++ Source/NWheels.Api/Configuration/ConfigElementAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NWheels.Configuration
+{
+    /// <summary>
+    /// Applies to an interfaces that represent configuration elements
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = true)]
+    public class ConfigElementAttribute : Attribute
+    {
+    }
+}
---
+++ Source/NWheels.Api/Configuration/ConfigSectionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NWheels.Configuration
+{
+    /// <summary>
+    /// Applies to an interfaces that represent configuration sections
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = true)]
+    public class ConfigSectionAttribute : Attribute
+    {
+    }
+}
