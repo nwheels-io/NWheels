ac648e75622a930138765b583c920ac83ec74be4
felix-b <felixberman76@gmail.com>
2017-09-04 08:39:26 +0300

    Restructured solution into subfolders

---
+++ Source2/Stacks/DB/Mongo/NWheels.DB.Adapters.Mongo.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NWheels.DB.Adapters.Mongo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
