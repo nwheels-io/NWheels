6a863c0a3e00b8d4dc1d627850ce3da5e862218b
felix-b <felixberman76@gmail.com>
2017-09-11 08:56:24 +0300

    Core/Kernel: adding missing tests - WIP

--- Source2/Core/Kernel/NWheels.Kernel.UnitTests/Runtime/Injection/ComponentContainerTests.cs
+++ Source2/Core/Kernel/NWheels.Kernel.UnitTests/Runtime/Injection/ComponentContainerTests.cs
@@ -56,6 +56,376 @@ namespace NWheels.Kernel.UnitTests.Runtime.Injection
         //-----------------------------------------------------------------------------------------------------------------------------------------------------
 
         [Fact]
+        public void CanResolveComponentByConcreteType()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentA>();
+            builder.RegisterComponentType<ComponentB>();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedA = containerUnderTest.Resolve<ComponentA>();
+            var resolvedB = containerUnderTest.Resolve<ComponentB>();
+
+            //-- assert
+
+            resolvedA.Should().NotBeNull();
+            resolvedA.Should().BeOfType<ComponentA>();
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanResolveComponentByService()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentA>().ForService<IServiceA>();
+            builder.RegisterComponentType<ComponentB>().ForService<IServiceB>();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedA = containerUnderTest.Resolve<IServiceA>();
+            var resolvedB = containerUnderTest.Resolve<IServiceB>();
+
+            //-- assert
+
+            resolvedA.Should().NotBeNull();
+            resolvedA.Should().BeOfType<ComponentA>();
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanRegisterComponentForTwoServices()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentA>().ForServices<IServiceA, IAnyComponent>();
+            builder.RegisterComponentType<ComponentB>().ForServices<IServiceB, IAnyComponent>();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedA = containerUnderTest.Resolve<IServiceA>();
+            var resolvedB = containerUnderTest.Resolve<IServiceB>();
+            var resolvedAny = containerUnderTest.ResolveAll<IAnyComponent>().ToArray();
+
+            //-- assert
+
+            resolvedA.Should().NotBeNull();
+            resolvedA.Should().BeOfType<ComponentA>();
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+
+            resolvedAny.Should().NotBeNull();
+            resolvedAny.Select(r => r.GetType()).Should().BeEquivalentTo(typeof(ComponentA), typeof(ComponentB));
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanRegisterComponentForThreeServices()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentA>().ForServices<IServiceA, IAnyComponent>();
+            builder.RegisterComponentType<ComponentB>().ForServices<IServiceB, ISpecialComponent, IAnyComponent>();
+            builder.RegisterComponentType<ComponentC>().ForServices<IServiceC, ISpecialComponent, IAnyComponent>();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedA = containerUnderTest.Resolve<IServiceA>();
+            var resolvedB = containerUnderTest.Resolve<IServiceB>();
+            var resolvedSpecial = containerUnderTest.ResolveAll<ISpecialComponent>().ToArray();
+
+            //-- assert
+
+            resolvedA.Should().NotBeNull();
+            resolvedA.Should().BeOfType<ComponentA>();
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+
+            resolvedSpecial.Should().NotBeNull();
+            resolvedSpecial.Select(r => r.GetType()).Should().BeEquivalentTo(typeof(ComponentB), typeof(ComponentC));
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanRegisterComponentForMultipleServices_NonGeneric()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentA>().ForServices(typeof(IServiceA), typeof(IAnyComponent));
+            builder.RegisterComponentType<ComponentB>().ForServices(typeof(IServiceB), typeof(IAnyComponent));
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedA = containerUnderTest.Resolve<IServiceA>();
+            var resolvedB = containerUnderTest.Resolve<IServiceB>();
+            var resolvedAny = containerUnderTest.ResolveAll<IAnyComponent>().ToArray();
+
+            //-- assert
+
+            resolvedA.Should().NotBeNull();
+            resolvedA.Should().BeOfType<ComponentA>();
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+
+            resolvedAny.Should().NotBeNull();
+            resolvedAny.Select(r => r.GetType()).Should().BeEquivalentTo(typeof(ComponentA), typeof(ComponentB));
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanRegisterComponentType_NonGeneric()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType(typeof(ComponentA)).ForService<IServiceA>();
+            builder.RegisterComponentType(typeof(ComponentB)).ForService<IServiceB>();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedA = containerUnderTest.Resolve<IServiceA>();
+            var resolvedB = containerUnderTest.Resolve<IServiceB>();
+
+            //-- assert
+
+            resolvedA.Should().NotBeNull();
+            resolvedA.Should().BeOfType<ComponentA>();
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanRegisterNamedComponentForTwoServices()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentB>().NamedForServices<ISpecialComponent, IAnyComponent>("BBB");
+            builder.RegisterComponentType<ComponentC>().NamedForServices<ISpecialComponent, IAnyComponent>("CCC");
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedB = containerUnderTest.ResolveNamed<ISpecialComponent>("BBB");
+            var resolvedC = containerUnderTest.ResolveNamed<IAnyComponent>("CCC");
+
+            //-- assert
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+
+            resolvedC.Should().NotBeNull();
+            resolvedC.Should().BeOfType<ComponentC>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanRegisterNamedComponentForThreeServices()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentB>().NamedForServices<IServiceB, ISpecialComponent, IAnyComponent>("BBB");
+            builder.RegisterComponentType<ComponentC>().NamedForServices<IServiceC, ISpecialComponent, IAnyComponent>("CCC");
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedB = containerUnderTest.ResolveNamed<IServiceB>("BBB");
+            var resolvedC = containerUnderTest.ResolveNamed<ISpecialComponent>("CCC");
+
+            //-- assert
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+
+            resolvedC.Should().NotBeNull();
+            resolvedC.Should().BeOfType<ComponentC>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanRegisterNamedComponentForMultipleServices_NonGeneric()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentB>().NamedForServices("BBB", typeof(ISpecialComponent), typeof(IAnyComponent));
+            builder.RegisterComponentType<ComponentC>().NamedForServices("CCC", typeof(ISpecialComponent), typeof(IAnyComponent));
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolvedB = containerUnderTest.ResolveNamed<ISpecialComponent>("BBB");
+            var resolvedC = containerUnderTest.ResolveNamed<IAnyComponent>("CCC");
+
+            //-- assert
+
+            resolvedB.Should().NotBeNull();
+            resolvedB.Should().BeOfType<ComponentB>();
+
+            resolvedC.Should().NotBeNull();
+            resolvedC.Should().BeOfType<ComponentC>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void RegisterComponentAsFallback_NoOverrideRegistered_FallbackResolved()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentB>().ForService<ISpecialComponent>().AsFallback();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolved = containerUnderTest.Resolve<ISpecialComponent>();
+
+            //-- assert
+
+            resolved.Should().NotBeNull();
+            resolved.Should().BeOfType<ComponentB>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void RegisterComponentAsFallback_OverrideRegistered_OverrideResolved()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentB>().ForService<ISpecialComponent>().AsFallback();
+            builder.RegisterComponentType<ComponentC>().ForService<ISpecialComponent>();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolved = containerUnderTest.Resolve<ISpecialComponent>();
+
+            //-- assert
+
+            resolved.Should().BeOfType<ComponentC>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void CanResolveNamedComponent()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentA>().NamedForService<IAnyComponent>("AAA");
+            builder.RegisterComponentType<ComponentB>().NamedForService<IAnyComponent>("BBB");
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            var resolved = containerUnderTest.ResolveNamed<IAnyComponent>("BBB");
+
+            //-- assert
+
+            resolved.Should().NotBeNull();
+            resolved.Should().BeOfType<ComponentB>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void TryResolve_ComponentRegistered_Resolved()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentA>().ForService<IServiceA>();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            IServiceA resolved;
+            var result = containerUnderTest.TryResolve<IServiceA>(out resolved);
+
+            //-- assert
+
+            result.Should().BeTrue();
+            resolved.Should().NotBeNull();
+            resolved.Should().BeOfType<ComponentA>();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void TryResolve_ComponentNotRegistered_NullReturned()
+        {
+            //-- arrange
+
+            var builder = new ComponentContainerBuilder();
+            builder.RegisterComponentType<ComponentA>().ForService<IServiceA>();
+
+            var containerUnderTest = builder.CreateComponentContainer(isRootContainer: true);
+
+            //-- act
+
+            IServiceB resolved;
+            var result = containerUnderTest.TryResolve<IServiceB>(out resolved);
+
+            //-- assert
+
+            result.Should().BeFalse();
+            resolved.Should().BeNull();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Fact]
         public void Merge_Resolve_ReturnsLastComponentRegisteredForService()
         {
             //-- arrange
@@ -111,10 +481,25 @@ namespace NWheels.Kernel.UnitTests.Runtime.Injection
         public interface IAnyComponent
         {
         }
-        public class ComponentA : IAnyComponent
+        public interface ISpecialComponent
         {
         }
-        public class ComponentB : IAnyComponent
+        public interface IServiceA
+        {
+        }
+        public interface IServiceB
+        {
+        }
+        public interface IServiceC
+        {
+        }
+        public class ComponentA : IServiceA, IAnyComponent
+        {
+        }
+        public class ComponentB : IServiceB, ISpecialComponent, IAnyComponent
+        {
+        }
+        public class ComponentC : IServiceC, ISpecialComponent, IAnyComponent
         {
         }
     }
---
+++ Source2/Core/Kernel/NWheels.Kernel.UnitTests/Runtime/Injection/PortsAndAdaptersPoc.cs
@@ -0,0 +1,215 @@
+﻿using Autofac;
+using FluentAssertions;
+using NWheels.Injection;
+using NWheels.Microservices;
+using NWheels.Runtime.Injection;
+using NWheels.Testability;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace NWheels.Kernel.UnitTests.Runtime.Injection
+{
+    public class PortsAndAdaptersPoc : TestBase.UnitTest
+    {
+        [Fact]
+        public void CanConnectsPortsAndAdapters()
+        {
+            //-- arrange
+
+            var container = new ComponentContainerBuilder().CreateComponentContainer(isRootContainer: true);
+            var builder = new ComponentContainerBuilder();
+            var features = new List<IFeatureLoader> {
+                new ExampleDependencyFeatureLoader(),
+                new ExamplePortFeatureLoader(),
+                new ExampleAdapterFeatureLoader(),
+            };
+
+            //-- act
+
+            features.ForEach(f => f.ContributeComponents(container, builder));
+
+            container.Merge(builder);
+            var adapterBuilder = new ComponentContainerBuilder();
+
+            features.ForEach(f => f.ContributeAdapterComponents(container, adapterBuilder));
+
+            container.Merge(adapterBuilder);
+
+            //-- assert
+
+            var allAdaptersA = container.ResolveAll<IExampleAdapterA>().ToArray();
+            var allAdaptersB = container.ResolveAll<IExampleAdapterB>().ToArray();
+            var allLifecycleListeners = container.ResolveAll<ITestLifecycleComponent>().ToArray();
+
+            allAdaptersA.Select(a => a.QualifiedValue).Should().BeEquivalentTo("DPY_ABC", "DPY_DEF");
+            allAdaptersB.Select(a => a.QualifiedValue).Should().BeEquivalentTo("DPY_ZZZ");
+
+            allLifecycleListeners.OfType<IExampleAdapterA>().Should().BeEquivalentTo(allAdaptersA);
+            allLifecycleListeners.OfType<IExampleAdapterB>().Should().BeEquivalentTo(allAdaptersB);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class ExamplePortA : InjectorPort
+        {
+            public ExamplePortA(IComponentContainerBuilder containerBuilder) : base(containerBuilder)
+            {
+            }
+
+            public string Value { get; set; }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class ExamplePortB : InjectorPort
+        {
+            public ExamplePortB(IComponentContainerBuilder containerBuilder) : base(containerBuilder)
+            {
+            }
+
+            public string Value { get; set; }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public interface IExampleAdapterA
+        {
+            string QualifiedValue { get; }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public interface IExampleAdapterB
+        {
+            string QualifiedValue { get; }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public interface ITestLifecycleComponent
+        {
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class ExampleAdapterA : ITestLifecycleComponent, IExampleAdapterA
+        {
+            private readonly ExampleDependency _dependnecy;
+            private readonly string _value;
+
+            public ExampleAdapterA(ExampleDependency dependency, ExamplePortA port)
+            {
+                _dependnecy = dependency;
+                _value = port.Value;
+            }
+
+            public string QualifiedValue => $"{_dependnecy.Prefix}_{_value}";
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class ExampleAdapterB : ITestLifecycleComponent, IExampleAdapterB
+        {
+            private readonly ExampleDependency _dependnecy;
+            private readonly string _value;
+
+            public ExampleAdapterB(ExampleDependency dependency, ExamplePortB port)
+            {
+                _dependnecy = dependency;
+                _value = port.Value;
+            }
+
+            public string QualifiedValue => $"{_dependnecy.Prefix}_{_value}";
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class ExampleDependency
+        {
+            public string Prefix => "DPY";
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class ExampleDependencyFeatureLoader : FeatureLoaderBase
+        {
+            public override void ContributeComponents(IComponentContainer existingComponents, IComponentContainerBuilder newComponents)
+            {
+                base.ContributeComponents(existingComponents, newComponents);
+                newComponents.RegisterComponentType<ExampleDependency>().SingleInstance();//typeof(ExampleDependency), LifeStyle.Singleton);
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class ExamplePortFeatureLoader : FeatureLoaderBase
+        {
+            public override void ContributeComponents(IComponentContainer existingComponents, IComponentContainerBuilder newComponents)
+            {
+                base.ContributeComponents(existingComponents, newComponents);
+
+                newComponents.ContributePortAExample("ABC");
+                newComponents.ContributePortAExample("DEF");
+                newComponents.ContributePortBExample("ZZZ");
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public class ExampleAdapterFeatureLoader : FeatureLoaderBase
+        {
+            public override void ContributeAdapterComponents(IComponentContainer existingComponents, IComponentContainerBuilder newComponents)
+            {
+                base.ContributeAdapterComponents(existingComponents, newComponents);
+
+                var allPortsA = existingComponents.ResolveAll<ExamplePortA>();
+
+                foreach (var port in allPortsA)
+                {
+                    newComponents.RegisterComponentType<ExampleAdapterA>()
+                        .WithParameter<ExamplePortA>(port)
+                        .SingleInstance()
+                        .ForServices<IExampleAdapterA, ITestLifecycleComponent>();
+                }
+
+                var allPortsB = existingComponents.ResolveAll<ExamplePortB>();
+
+                foreach (var port in allPortsB)
+                {
+                    newComponents.RegisterComponentType<ExampleAdapterB>()
+                        .WithParameter<ExamplePortB>(port)
+                        .SingleInstance()
+                        .ForServices<IExampleAdapterB, ITestLifecycleComponent>();
+                }
+            }
+        }
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public static class TestContainerBuilderExtensions
+    {
+        public static void ContributePortAExample(this IComponentContainerBuilder containerBuilder, string value)
+        {
+            containerBuilder.RegisterComponentInstance<PortsAndAdaptersPoc.ExamplePortA>(
+                new PortsAndAdaptersPoc.ExamplePortA(containerBuilder) {
+                    Value = value
+                }
+            );
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public static void ContributePortBExample(this IComponentContainerBuilder containerBuilder, string value)
+        {
+            containerBuilder.RegisterComponentInstance<PortsAndAdaptersPoc.ExamplePortB>(
+                new PortsAndAdaptersPoc.ExamplePortB(containerBuilder) {
+                    Value = value
+                }
+            );
+        }
+    }
+}
