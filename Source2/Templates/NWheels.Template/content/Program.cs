6b5ce4b6ae931652fac22cf891de392437bd74e1
felix-b <felixberman76@gmail.com>
2017-09-29 16:00:52 +0300

    Added microservice template package for 'dotnet new'.

---
+++ Source2/Templates/NWheels.Template/content/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NWheels.Microservices;
+using NWheels.Microservices.Api;
+using NWheels.Microservices.Runtime;
+
+namespace My.Service
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return new MicroserviceHostBuilder("ServiceName")
+                .UseLifecycleComponent<HelloWorldComponent>()
+                .RunCli(args);
+        }
+
+        public class HelloWorldComponent : LifecycleComponentBase
+        {
+            public override void MicroserviceActivated()
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
