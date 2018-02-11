b6a450c714169c96e3f56ff743b9ff2746584661
felix-b <felixberman76@gmail.com>
2018-02-11 21:27:19 +0200

    Electricity billing sample -- initial commit

---
+++ Samples/02-ElectricityBilling/ElectricityBilling/ElectricityBilling.BillingService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElectricityBilling.BillingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
