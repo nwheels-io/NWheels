d790c10cda8495c402cfe0c1f485d024c63c0980
felix-b <felixberman76@gmail.com>
2018-03-27 22:30:46 +0300

    Samples/TodoList: adding CSS to theme

--- Samples/01-TodoList/webui/hello2/src/themes/ascet/Counter/render.js
+++ Samples/01-TodoList/webui/hello2/src/themes/ascet/Counter/render.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import * as Counter from './../../../components/Counter'
+import './style.css';
+import classnames from 'classnames'
 
 export const UIRender = (props, actions, context) => (
-    <React.Fragment>
+    <div className={classnames('Component_Counter', {
+        'positive': props.delta > 0,
+        'negative': props.delta < 0
+    })}>
         <span>Initial:</span>
         <span><strong>{props.initialValue}</strong></span>
 
@@ -12,7 +17,7 @@ export const UIRender = (props, actions, context) => (
         <span>Delta:</span>
         <span><strong>{props.delta}</strong></span>
 
-        <button onClick={() => actions.incrementOne()}>Increment ({props.increments})</button>
-        <button onClick={() => actions.decrementOne()}>Decrement ({props.decrements})</button>
-    </React.Fragment>
+        <button className="increment" onClick={() => actions.incrementOne()}>Increment ({props.increments})</button>
+        <button className="decrement" onClick={() => actions.decrementOne()}>Decrement ({props.decrements})</button>
+    </div>
 )
